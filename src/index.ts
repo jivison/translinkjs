7d18b144ba1b91ae05e2ec756afb96adeadc76e3
jivison <john@ivison.org>
2022-05-24 23:28:08 -0700

    Make changes for npm

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { RTTI } from "./rtti";
+export { RTTIAPIError } from "./types/errors";
+export { Coordinates, StopsParams, BusesParams } from "./types/apiParams";
+export * from "./structures/BusDetails";
+export * from "./structures/NextBus";
+export * from "./structures/RouteDetails";
+export * from "./structures/RoutePattern";
+export * from "./structures/StopDetails";
